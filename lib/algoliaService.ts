3b1ea3ba4d289a4a855fd925a14eef27c0082eff
Lucas Harada <lucasharada5@gmail.com>
2022-10-01 15:09:32 -0300

    Update algoliaService.ts

--- lib/algoliaService.ts
+++ lib/algoliaService.ts
@@ -1,7 +1,7 @@
 import algoliasearch from "algoliasearch/lite";
 
-const APPLICATION_ID = "EBK4SL19S7";
-const SEARCH_API_KEY = "55e244d9faa873e5295388a961e3b3ea";
+const APPLICATION_ID = "PY17S3DJJE";
+const SEARCH_API_KEY = "0963d1bb0cdb44a8c33f0a00013ddea9";
 
 const client = algoliasearch(APPLICATION_ID, SEARCH_API_KEY);
 
